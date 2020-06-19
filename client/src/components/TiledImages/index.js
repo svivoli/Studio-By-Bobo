6f7f6b79f5f2f81fddcf7008b5da4cf66f01eb54
Sarah Vivoli <snvivoli@gmail.com>
2020-06-19 12:14:33 -0700

    all div components added to tiled images

--- client/src/components/TiledImages/index.js
+++ client/src/components/TiledImages/index.js
@@ -16,71 +16,183 @@ const TiledImages = () => (
             </Col>
             <Col className="gutter-row" span={8}>
                 <Div>
-                <img src={require('../../img/2.png')}></img>
-                <Div2>
-                    <p>title</p>
-                    <p>medium</p>
-                    <a className="info-link" href="#">www.etsylink.com</a>
-                </Div2>
+                    <img src={require('../../img/2.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
                 </Div>
             </Col>
             <Col className="gutter-row" span={8}>
-                <img src={require('../../img/3.png')}></img>
+                <Div>
+                    <img src={require('../../img/3.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
             </Col>
         </Row>
         <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
             <Col className="gutter-row" span={8}>
-                <img src={require('../../img/4.png')}></img>
+                <Div>
+                    <img src={require('../../img/4.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
             </Col>
             <Col className="gutter-row" span={8}>
-                <img src={require('../../img/5.png')}></img>
+                <Div>
+                    <img src={require('../../img/5.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
             </Col>
             <Col className="gutter-row" span={8}>
-                <img src={require('../../img/6.png')}></img>
-            </Col>
-        </Row>
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={8}>
-                <img src={require('../../img/7.png')}></img>
-            </Col>
-            <Col className="gutter-row" span={8}>
-                <img src={require('../../img/8.png')}></img>
-            </Col>
-            <Col className="gutter-row" span={8}>
-                <img src={require('../../img/9.png')}></img>
-            </Col>
-        </Row>
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={8}>
-                <img src={require('../../img/10.png')}></img>
-            </Col>
-            <Col className="gutter-row" span={8}>
-                <img src={require('../../img/11.png')}></img>
-            </Col>
-            <Col className="gutter-row" span={8}>
-                <img src={require('../../img/12.png')}></img>
-            </Col>
-        </Row>
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={8}>
-                <img src={require('../../img/13.png')}></img>
-            </Col>
-            <Col className="gutter-row" span={8}>
-                <img src={require('../../img/14.png')}></img>
-            </Col>
-            <Col className="gutter-row" span={8}>
-                <img src={require('../../img/15.png')}></img>
+                <Div>
+                    <img src={require('../../img/6.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
             </Col>
         </Row>
         <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
             <Col className="gutter-row" span={8}>
-                <img src={require('../../img/16.png')}></img>
+                <Div>
+                    <img src={require('../../img/7.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
             </Col>
             <Col className="gutter-row" span={8}>
-                <img src={require('../../img/17.png')}></img>
+                <Div>
+                    <img src={require('../../img/8.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
             </Col>
             <Col className="gutter-row" span={8}>
-                <img src={require('../../img/18.png')}></img>
+                <Div>
+                    <img src={require('../../img/9.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
+            </Col>
+        </Row>
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col className="gutter-row" span={8}>
+                <Div>
+                    <img src={require('../../img/10.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
+            </Col>
+            <Col className="gutter-row" span={8}>
+                <Div>
+                    <img src={require('../../img/11.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
+            </Col>
+            <Col className="gutter-row" span={8}>
+                <Div>
+                    <img src={require('../../img/12.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
+            </Col>
+        </Row>
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col className="gutter-row" span={8}>
+                <Div>
+                    <img src={require('../../img/13.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
+            </Col>
+            <Col className="gutter-row" span={8}>
+                <Div>
+                    <img src={require('../../img/14.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
+            </Col>
+            <Col className="gutter-row" span={8}>
+                <Div>
+                    <img src={require('../../img/15.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
+            </Col>
+        </Row>
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col className="gutter-row" span={8}>
+                <Div>
+                    <img src={require('../../img/16.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
+            </Col>
+            <Col className="gutter-row" span={8}>
+                <Div>
+                    <img src={require('../../img/17.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
+            </Col>
+            <Col className="gutter-row" span={8}>
+                <Div>
+                    <img src={require('../../img/18.png')}></img>
+                    <Div2>
+                        <p>title</p>
+                        <p>medium</p>
+                        <a className="info-link" href="#">www.etsylink.com</a>
+                    </Div2>
+                </Div>
             </Col>
         </Row>
     </div>
@@ -100,9 +212,10 @@ class Div extends React.Component {
 class Div2 extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             color: 'transparent',
-            background: 'transparent' };
+            background: 'transparent'
+        };
 
         this.handleMouseEnter = this.handleMouseEnter.bind(this);
         this.handleMouseLeave = this.handleMouseLeave.bind(this);
@@ -110,7 +223,7 @@ class Div2 extends React.Component {
 
     handleMouseEnter = () => {
         this.setState({ color: 'black' });
-        this.setState({ background: 'rgba(80, 80, 80, 0.3)'});
+        this.setState({ background: 'rgba(80, 80, 80, 0.3)' });
         console.log(this.state.color);
     }
 
@@ -121,7 +234,7 @@ class Div2 extends React.Component {
 
     render() {
         return (
-            <div className="info" style={{ color: `${this.state.color}`,  backgroundColor: `${this.state.background}`}} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
+            <div className="info" style={{ color: `${this.state.color}`, backgroundColor: `${this.state.background}` }} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
                 {this.props.children}
             </div>
         )
