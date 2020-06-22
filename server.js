0062238a631b415fb7bc7e84217619c9efdd01fb
Sarah Vivoli <snvivoli@gmail.com>
2020-06-22 12:30:44 -0700

    build

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-  }
+}
 
 app.listen(PORT, function () {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
