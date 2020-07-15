ce0e0a84a496fdf33cc0a97311c0f397edc85dc1
Sarah Vivoli <snvivoli@gmail.com>
2020-07-15 11:33:16 -0700

    mediums added to tiled images

--- client/src/components/TiledImages/index.js
+++ client/src/components/TiledImages/index.js
@@ -9,7 +9,7 @@ const TiledImages = () => (
                     <img src={require('../../img/fleurs.png')}></img>
                     <Div2>
                         <p>vase of ranunculuses</p>
-                        <p>medium</p>
+                        <p>ink and gouache on paper</p>
                         <a className="info-link" href="https://www.etsy.com/listing/809087050/vase-of-ranunculuses?ref=shop_home_active_9&frs=1" target="_blank">purchase on Etsy</a>
                     </Div2>
                 </Div>
@@ -19,7 +19,7 @@ const TiledImages = () => (
                     <img src={require('../../img/girlatcafe.png')}></img>
                     <Div2>
                         <p>girl at cafe</p>
-                        <p>medium</p>
+                        <p>ink and gouache on paper</p>
                         <a className="info-link" href="https://www.etsy.com/listing/797407812/girl-at-cafe?ref=shop_home_active_24&frs=1" target="_blank">purchase on Etsy</a>
                     </Div2>
                 </Div>
@@ -32,7 +32,7 @@ const TiledImages = () => (
                     <img src={require('../../img/girlsitting2019.png')}></img>
                     <Div2>
                         <p>girl sitting</p>
-                        <p>medium</p>
+                        <p>ink and gouache on paper</p>
                         <a className="info-link" href="https://www.etsy.com/listing/822984031/girl-sitting?ref=shop_home_active_10&frs=1" target="_blank">purchase on Etsy</a>
                     </Div2>
                 </Div>
@@ -42,7 +42,7 @@ const TiledImages = () => (
                     <img src={require('../../img/girlwithfruit.png')}></img>
                     <Div2>
                         <p>girl holding fruit</p>
-                        <p>medium</p>
+                        <p>ink and gouache on paper</p>
                         <a className="info-link" href="https://www.etsy.com/listing/809084410/girl-holding-fruit?ref=shop_home_active_11&frs=1" target="_blank">purchase on Etsy</a>
                     </Div2>
                 </Div>
@@ -54,7 +54,7 @@ const TiledImages = () => (
                     <img src={require('../../img/girlinpool2019.png')}></img>
                     <Div2>
                         <p>girl in pool</p>
-                        <p>medium</p>
+                        <p>ink and gouache on paper</p>
                         {/* <a className="info-link" href="#" target="_blank">www.etsylink.com</a> */}
                     </Div2>
                 </Div>
@@ -64,7 +64,7 @@ const TiledImages = () => (
                     <img src={require('../../img/jaroflilliesandpoppies.png')}></img>
                     <Div2>
                         <p>jar of lillies and poppies</p>
-                        <p>medium</p>
+                        <p>ink and gouache on paper</p>
                         <a className="info-link" href="https://www.etsy.com/listing/812210140/jar-of-lilies-and-poppies?ref=shop_home_active_4&frs=1" target="_blank">purchase on Etsy</a>
                     </Div2>
                 </Div>
@@ -76,7 +76,7 @@ const TiledImages = () => (
                     <img src={require('../../img/stilllifeofvases.png')}></img>
                     <Div2>
                         <p>study of vases</p>
-                        <p>medium</p>
+                        <p>ink and gouache on paper</p>
                         <a className="info-link" href="https://www.etsy.com/listing/826109919/study-of-vases?ref=shop_home_active_5&frs=1" target="_blank">purchase on Etsy</a>
                     </Div2>
                 </Div>
@@ -86,7 +86,7 @@ const TiledImages = () => (
                     <img src={require('../../img/summerroses2020.png')}></img>
                     <Div2>
                         <p>summer roses</p>
-                        <p>medium</p>
+                        <p>ink and gouache on paper</p>
                         {/* <a className="info-link" href="#" target="_blank">www.etsylink.com</a> */}
                     </Div2>
                 </Div>
